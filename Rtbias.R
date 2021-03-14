76e9f3e339b5f253191b58501aaf98ffbe45a412
parksw3 <parksw3@mcmaster.ca>
2021-03-14 04:16:22 -0400

    done for now?

--- Rtbias.R
+++ Rtbias.R
@@ -12,32 +12,16 @@ source("renewal_det.R")
 
 theta <- 1.5
 genfun_assumed <- function(x) dgamma(x, 5, 5/5)
-genfun_short <- function(x) dgamma(x, 5, 5/4)
-genfun_long <- function(x) dgamma(x, 5, 5/6)
+genfun_short <- function(x) dgamma(x, 5, 5/3.5)
+genfun_long <- function(x) dgamma(x, 5, 5/6.5)
 
 svec <- c("Wild type GI = Variant GI = Assumed GI",
-          "Wild type GI = Variant GI < Assumed GI",
-          "Assumed GI < Wild type GI = Variant GI",
-          "Variant GI < Assumed GI < Wild type GI",
-          "Wild type GI < Assumed GI < Variant GI",
-          "Variant GI < Wild type GI = Assumed GI",
-          "Wild type GI = Assumed GI < Variant GI")
+          "Variant GI $<$ Wild type GI = Assumed GI",
+          "Wild type GI = Assumed GI $<$ Variant GI")
 
 slist <- list(
   list(genfun1=genfun_assumed,
        genfun2=genfun_assumed,
-       genfun3=genfun_assumed),
-  list(genfun1=genfun_short,
-       genfun2=genfun_short,
-       genfun3=genfun_assumed),
-  list(genfun1=genfun_long,
-       genfun2=genfun_long,
-       genfun3=genfun_assumed),
-  list(genfun1=genfun_long,
-       genfun2=genfun_short,
-       genfun3=genfun_assumed),
-  list(genfun1=genfun_short,
-       genfun2=genfun_long,
        genfun3=genfun_assumed),
   list(genfun1=genfun_assumed,
        genfun2=genfun_short,
@@ -56,18 +40,18 @@ for (i in 1:length(svec)) {
     tmax=150, slist[[i]])
   
   rr <- do.call(renewal_det, tmparg) %>%
-    filter(tvec > 15)
-  
+    filter(tvec > 15, tvec < 95)
   
   g1 <- ggplot(rr) +
     geom_line(aes(tvec, Ivec1, color="Wild type")) +
     geom_line(aes(tvec, Ivec2, color="Variant")) +
-    scale_x_continuous("Time (days)", expand=c(0, 0), limits=c(15, 110)) +
-    scale_y_continuous("Incidence (1/days)") +
+    scale_x_continuous("Time (days)", expand=c(0, 0), limits=c(15, 95)) +
+    scale_y_log10("Incidence (1/days)") +
     scale_color_manual(values=c("red", "black")) +
+    ggtitle(LETTERS[(i-1)*4+1]) +
     theme(
       panel.grid = element_blank(),
-      legend.position = c(0.75, 0.75),
+      legend.position = c(0.35, 0.25),
       legend.title = element_blank()
     )
   
@@ -80,9 +64,10 @@ for (i in 1:length(svec)) {
     geom_line(aes(tvec, Rt2, color="Variant")) +
     geom_line(aes(tvec, Rtest1, color="Wild type"), lty=2) +
     geom_line(aes(tvec, Rtest2, color="Variant"), lty=2) +
-    scale_x_continuous("Time (days)", expand=c(0, 0), limits=c(15, 145)) +
-    scale_y_continuous("Reproduction number") +
+    scale_x_continuous("Time (days)", expand=c(0, 0), limits=c(15, 95)) +
+    scale_y_continuous("Reproduction number, $\\mathcal{R}(t)$") +
     scale_color_manual(values=c("red", "black")) +
+    ggtitle(LETTERS[(i-1)*4+2]) +
     theme(
       panel.grid = element_blank(),
       legend.position = "none"
@@ -91,9 +76,10 @@ for (i in 1:length(svec)) {
   g3 <- ggplot(rr) +
     geom_line(aes(tvec, Rt2/Rt1, color="True ratio")) +
     geom_line(aes(tvec, Rtest2/Rtest1, color="Estimated ratio")) +
-    scale_x_continuous("Time (days)", expand=c(0, 0), limits=c(15, 145)) +
-    scale_y_continuous("Relative strength") +
+    scale_x_continuous("Time (days)", expand=c(0, 0), limits=c(15, 95)) +
+    scale_y_continuous("Relative strength, $\\theta(t)$") +
     scale_color_manual(values=c("orange", "purple")) +
+    ggtitle(LETTERS[(i-1)*4+3]) +
     theme(
       panel.grid = element_blank(),
       legend.position = c(0.7, 0.25),
@@ -106,11 +92,12 @@ for (i in 1:length(svec)) {
   
   g4 <- ggplot(rr) +
     geom_abline(intercept=0, slope=1, lty=2) +
-    geom_abline(intercept=0, slope=theta, lty=3) +
+    geom_abline(intercept=log10(theta), slope=1, lty=3) +
     geom_path(aes(Rtest1, Rtest2), col="orange") +
-    scale_x_continuous("Estimated wild type strength", limits=c(0, 2.6), expand=c(0, 0)) +
-    scale_y_continuous("Estimated variant strength", limits=c(0, 2.6*theta), expand=c(0, 0)) +
+    scale_x_log10("Estimated wild type strength, $\\hat{\\mathcal{R}}_w(t)$", limits=c(0.01, 4), expand=c(0, 0)) +
+    scale_y_log10("Estimated variant strength, $\\hat{\\mathcal{R}}_v(t)$", limits=c(0.01, 4*theta), expand=c(0, 0)) +
     scale_color_manual(values=c("orange", "purple")) +
+    ggtitle(LETTERS[(i-1)*4+4]) +
     theme(
       panel.grid = element_blank(),
       legend.position = c(0.7, 0.2),
@@ -119,10 +106,10 @@ for (i in 1:length(svec)) {
   
   gtot <- egg::ggarrange(g1, g2, g3, g4, nrow=1, draw=FALSE)
   
-  
-  glist[[i]] <- annotate_figure(gtot, top=paste0(LETTERS[i], ". ", svec[i]))
+  glist[[i]] <- annotate_figure(gtot, top=svec[i])
 }
 
-gfinal <- do.call(arrangeGrob, c(glist, ncol=1))
-
-ggsave("Rtbias.pdf", gfinal, width=12, height=16)
+tikz(file = "Rtbias.tex", width = 12, height = 8, standAlone = T)
+do.call(grid.arrange, c(glist, ncol=1))
+dev.off()
+tools::texi2dvi('Rtbias.tex', pdf = T, clean = T)
