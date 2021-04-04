14f9d5524b312ad093d9ae43c675031a08cf994f
parksw3 <parksw3@mcmaster.ca>
2021-04-04 01:11:38 -0400

    editing

--- relspeed.R
+++ relspeed.R
@@ -4,105 +4,63 @@ library(ggthemes)
 library(egg)
 library(tikzDevice)
 
-rho <- 1.61
-
-Rwvec <- seq(0.3, 1.7, length.out=31)
-kappavec <- c(0, 0.2, 0.5, 1)
-kappabase <- 0.2
-
-pardata <- expand.grid(kappavec, Rwvec)
-
+rw <- 0
+delta <- 0.1
+kappa_base <- 1/5
 Gw <- 5
+
+rho <- ((1 + kappa_base * (rw+delta) * Gw)^(1/kappa_base))
+
+Rwvec <- c(0.5/rho, 1/rho, 1, rho)
+kappavec <- c(0, 0.2, 0.5, 1)
 
 Gratio <- exp(seq(log(1/1.5), log(1.5), length.out=31))
 
-pardata2 <- expand.grid(Rwvec, Gratio)
+pardata <- expand.grid(kappavec, Rwvec, Gratio)
 
 deltadata <- apply(pardata, 1, function(x) {
   kappa <- x[[1]]
   Rw <- x[[2]]
+  Gratio <- x[[3]]
+  Gv <- Gratio * Gw
   
   if (kappa == 0) {
-    deltahat <- log(rho * Rw)/Gw - log(Rw)/Gw
+    delta <- log(rho * Rw)/Gv - log(Rw)/Gw
   } else {
-    deltahat <- ((rho * Rw)^kappa - 1)/(kappa*Gw) -  ((Rw)^kappa - 1)/(kappa*Gw)
+    delta <- ((rho * Rw)^kappa - 1)/(kappa*Gv) -  ((Rw)^kappa - 1)/(kappa*Gw)
   }
   
   data.frame(
     rho=rho,
     Rw=Rw,
-    deltahat=deltahat,
-    kappa=kappa
-  )
-}) %>%
-  bind_rows
-
-deltadata2 <- apply(pardata2, 1, function(x) {
-  Rw <- x[[1]]
-  Gratio <- x[[2]] ## Gv/Gw
-  Gv <- Gratio * Gw
-  
-  delta <- ((rho * Rw)^kappabase - 1)/(kappabase*Gv) -  ((Rw)^kappabase - 1)/(kappabase*Gw)
-  
-  deltahat <- ((rho * Rw)^kappabase - 1)/(kappabase*Gw) -  ((Rw)^kappabase - 1)/(kappabase*Gw)
-  
-  data.frame(
-    rho=rho,
-    Gratio=Gratio,
-    Rw=Rw,
     delta=delta,
-    deltahat=deltahat,
-    bias=delta-deltahat
+    kappa=kappa,
+    Gratio=Gratio
   )
 }) %>%
-  bind_rows
+  bind_rows %>%
+  mutate(
+    kappa=factor(kappa, levels=c(0, 0.2, 0.5, 1)),
+    Rw=factor(Rw, levels=Rwvec,
+              labels=c("$\\mathcal{R}_{\\textrm{wt}} < \\mathcal{R}_{\\textrm{var}} < 1$",
+                       "$\\mathcal{R}_{\\textrm{wt}} < \\mathcal{R}_{\\textrm{var}} = 1$",
+                       "$1 = \\mathcal{R}_{\\textrm{wt}} < \\mathcal{R}_{\\textrm{var}}$",
+                       "$1 < \\mathcal{R}_{\\textrm{wt}} < \\mathcal{R}_{\\textrm{var}}$"))
+  )
 
 g1 <- ggplot(deltadata) +
-  geom_line(aes(Rw, deltahat, lty=as.factor(kappa), col=as.factor(kappa))) +
-  scale_x_continuous("Wild type strength, $\\mathcal{R}_w$", expand=c(0, 0)) +
-  scale_y_continuous("Estimated relative speed, $\\hat{\\delta}$ (1/days)", limits=c(0, 0.33), expand=c(0, 0)) +
+  geom_line(aes(Gratio, delta, lty=kappa, col=kappa)) +
+  scale_x_log10("Relative mean generation interval, $\\bar{G}_{\\mathrm{var}}/\\bar{G}_{\\mathrm{wt}}$",
+                breaks=c(0.67, 1, 1.5)) +
+  scale_y_continuous("Relative speed, $\\delta$") +
+  facet_wrap(~Rw) +
   scale_color_colorblind("$\\kappa$") +
   scale_linetype_discrete("$\\kappa$") +
-  ggtitle("A. Equal generation intervals") +
-  theme(
-    panel.grid = element_blank(),
-    legend.position = c(0.2, 0.75)
-  )
-
-g2 <- ggplot(deltadata2) +
-  geom_tile(aes(Gratio, Rw, fill=delta)) +
-  geom_vline(xintercept=1, lty=2, col="white") +
-  geom_hline(yintercept=1/rho, lty=1, col="white") +
-  annotate("text", x=1.04, y=1.1, label=c("$\\bar{G}_v=\\bar{G}_w$"), col="white", angle=-90) +
-  annotate("text", x=0.8, y=1/rho+0.05, label=c("$\\mathcal{R}_v=1$"), col="white") +
-  scale_x_log10("Generation interval ratio, $\\bar{G}_{\\mathrm{var}}/\\bar{G}_{\\mathrm{wt}}$", expand=c(0, 0),
-                breaks=c(0.67, 1, 1.5)) +
-  scale_y_continuous("Wild type strength, $\\mathcal{R}_w$", expand=c(0, 0)) +
-  scale_fill_viridis_c("$\\delta$") +
-  ggtitle("B. Different generation intervals") +
   theme(
     panel.grid = element_blank()
   )
 
-g3 <- ggplot(deltadata2) +
-  geom_tile(aes(Gratio, Rw, fill=bias)) +
-  geom_vline(xintercept=1, lty=2, col="white") +
-  geom_hline(yintercept=1/rho, lty=1, col="white") +
-  annotate("text", x=1.04, y=1.1, label=c("$\\bar{G}_v=\\bar{G}_w$"), col="white", angle=-90) +
-  annotate("text", x=0.8, y=1/rho+0.05, label=c("$\\mathcal{R}_v=1$"), col="white") +
-  scale_x_log10("Generation interval ratio, $\\bar{G}_{\\mathrm{var}}/\\bar{G}_{\\mathrm{wt}}$", expand=c(0, 0),
-                breaks=c(0.67, 1, 1.5)) +
-  scale_y_continuous("Wild type strength, $\\mathcal{R}_w$", expand=c(0, 0)) +
-  scale_fill_viridis_c("$\\delta-\\hat{\\delta}$", option="A") +
-  ggtitle("C. Bias") +
-  theme(
-    panel.grid = element_blank(),
-    plot.margin = unit(c(0,-2.5,0,0), "cm")
-  )
-
-gtot <- ggarrange(g1, g2, g3, nrow=1, draw=FALSE)
-
-tikz(file = "relspeed.tex", width = 12, height = 4, standAlone = T)
-gtot
+tikz(file = "relspeed.tex", width = 6*1.2, height = 4*1.2, standAlone = T)
+g1
 dev.off()
 tools::texi2dvi('relspeed.tex', pdf = T, clean = T)
--- relstrength.R
+++ relstrength.R
@@ -5,109 +5,62 @@ library(egg)
 library(tikzDevice)
 
 delta <- 0.1
-rho <- 1.61
 
-rwvec <- seq(-0.13, 0.13, length.out=31)
+rwvec <- c(-0.15, -0.1, 0, 0.1)
 kappavec <- c(0, 0.2, 0.5, 1)
-kappabase <- 0.2
-
-pardata <- expand.grid(kappavec, rwvec)
 
 Gw <- 5
 
 Gratio <- exp(seq(log(1/1.5), log(1.5), length.out=31))
 
-pardata2 <- expand.grid(rwvec, Gratio)
+pardata <- expand.grid(kappavec, rwvec, Gratio)
 
 rhodata <- apply(pardata, 1, function(x) {
   kappa <- x[[1]]
   rw <- x[[2]]
+  Gratio <- x[[3]]
+  Gv <- Gratio * Gw
   
   if (kappa==0) {
-    rhohat <- exp(Gw * (rw + delta))/exp(Gw * rw)
+    rho <- exp(Gv * (rw + delta))/exp(Gw * rw)
   } else {
-    rhohat <- ((1 + kappa * (rw + delta) * Gw)^(1/kappa))/
+    rho <- ((1 + kappa * (rw + delta) * Gv)^(1/kappa))/
       ((1 + kappa * rw * Gw)^(1/kappa))
   }
   
   data.frame(
     delta=delta,
     rw=rw,
-    rhohat=rhohat,
+    rho=rho,
+    Gratio=Gratio,
     kappa=kappa
   )
 }) %>%
-  bind_rows
-
-rhodata2 <- apply(pardata2, 1, function(x) {
-  rw <- x[[1]]
-  Gratio <- x[[2]] ## Gv/Gw
-  Gv <- Gratio * Gw
-  
-  rho <- ((1 + kappabase * (rw + delta) * Gv)^(1/kappabase))/
-    ((1 + kappabase * rw * Gw)^(1/kappabase))
-  
-  rhohat <- ((1 + kappabase * (rw + delta) * Gw)^(1/kappabase))/
-    ((1 + kappabase * rw * Gw)^(1/kappabase))
-  
-  data.frame(
-    delta=delta,
-    Gratio=Gratio,
-    rw=rw,
-    rho=rho,
-    rhohat=rhohat,
-    bias=rho/rhohat
+  bind_rows %>%
+  mutate(
+    kappa=factor(kappa, levels=c(0, 0.2, 0.5, 1)),
+    rw=factor(rw, levels=rwvec,
+              labels=c("$r_{\\textrm{wt}} < r_{\\textrm{var}} < 0$",
+                       "$r_{\\textrm{wt}} < r_{\\textrm{var}} = 0$",
+                       "$0 = r_{\\textrm{wt}} < r_{\\textrm{var}}$",
+                       "$0 < r_{\\textrm{wt}} < r_{\\textrm{var}}$"))
   )
-}) %>%
-  bind_rows
+
+range(filter(rhodata, !kappa %in% c(0, 1))$rho)
 
 g1 <- ggplot(rhodata) +
-  geom_line(aes(rw, rhohat, lty=as.factor(kappa), col=as.factor(kappa))) +
-  scale_x_continuous("Wild type speed, $r_{\\mathrm{wt}}$ (1/days)", expand=c(0, 0)) +
-  scale_y_continuous("Estimated relative strength, $\\hat{\\rho}$", limits=c(1, 2.7), expand=c(0, 0)) +
+  geom_line(aes(Gratio, rho, lty=kappa, col=kappa)) +
+  scale_x_log10("Relative mean generation interval, $\\bar{G}_{\\mathrm{var}}/\\bar{G}_{\\mathrm{wt}}$",
+                breaks=c(0.67, 1, 1.5)) +
+  scale_y_continuous("Relative strength, $\\rho$") +
+  facet_wrap(~rw) +
   scale_color_colorblind("$\\kappa$") +
   scale_linetype_discrete("$\\kappa$") +
-  ggtitle("A. Equal generation intervals") +
-  theme(
-    panel.grid = element_blank(),
-    legend.position = c(0.8, 0.75)
-  )
-
-g2 <- ggplot(rhodata2) +
-  geom_tile(aes(Gratio, rw, fill=rho)) +
-  geom_vline(xintercept=1, lty=2, col="white") +
-  geom_hline(yintercept=-delta, lty=1, col="white") +
-  annotate("text", x=1.04, y=0, label=c("$\\bar{G}_{\\mathrm{var}}=\\bar{G}_{\\mathrm{wt}}$"), col="white", angle=-90) +
-  annotate("text", x=0.8, y=-delta+0.01, label=c("$r_{\\mathrm{var}}=0$"), col="white") +
-  scale_x_log10("Generation interval ratio, $\\bar{G}_{\\mathrm{var}}/\\bar{G}_{\\mathrm{wt}}$", expand=c(0, 0),
-                breaks=c(0.67, 1, 1.5)) +
-  scale_y_continuous("Wild type speed, $r_{\\mathrm{wt}}$ (1/days)", expand=c(0, 0)) +
-  scale_fill_viridis_c("$\\rho$") +
-  ggtitle("B. Different generation intervals") +
   theme(
     panel.grid = element_blank()
   )
-  
-g3 <- ggplot(rhodata2) +
-  geom_tile(aes(Gratio, rw, fill=bias)) +
-  geom_vline(xintercept=1, lty=2, col="white") +
-  geom_hline(yintercept=-delta, lty=1, col="white") +
-  annotate("text", x=1.04, y=0, label=c("$\\bar{G}_{\\mathrm{var}}=\\bar{G}_{\\mathrm{wt}}$"), col="white", angle=-90) +
-  annotate("text", x=0.8, y=-delta+0.01, label=c("$r_{\\mathrm{var}}=0$"), col="white") +
-  scale_x_log10("Generation interval ratio, $\\bar{G}_{\\mathrm{var}}/\\bar{G}_{\\mathrm{wt}}$", expand=c(0, 0),
-                breaks=c(0.67, 1, 1.5)) +
-  scale_y_continuous("Wild type speed, $r_{\\mathrm{wt}}$ (1/days)", expand=c(0, 0)) +
-  scale_fill_viridis_c("$\\rho/\\hat{\\rho}$", option="A") +
-  ggtitle("C. Bias") +
-  theme(
-    panel.grid = element_blank(),
-    plot.margin = unit(c(0,-2,0,0), "cm")
-    
-  )
 
-gtot <- ggarrange(g1, g2, g3, nrow=1, draw=FALSE)
-
-tikz(file = "relstrength.tex", width = 12, height = 4, standAlone = T)
-gtot
+tikz(file = "relstrength.tex", width = 6*1.2, height = 4*1.2, standAlone = T)
+g1
 dev.off()
 tools::texi2dvi('relstrength.tex', pdf = T, clean = T)
