fd1e012516051e127e30f5735a568a4aec1752a4
parksw3 <parksw3@mcmaster.ca>
2021-07-30 19:53:41 -0400

    minor edits

--- control.R
+++ control.R
@@ -85,11 +85,60 @@ gendata_long <- data_frame(
   speed=density*exp(-constant_speed*tvec)
 )
 
-Gpost_long <- integrate(function(x) x*Rv_base*dgamma(x, shape=1/kappa, rate=1/kappa/(Gw*tail(Gratio,1)))*exp(-constant_speed*x),
+Gpost_long <- integrate(function(x) x*filter(intervention, Gratio==tail(Gratio,1))$Rv*dgamma(x, shape=1/kappa, rate=1/kappa/(Gw*tail(Gratio,1)))*exp(-constant_speed*x),
                         0,
                         Inf)[[1]]/filter(intervention, Gratio==tail(Gratio,1))$speed_post_strength
 
-g1 <- ggplot(gendata_base) +
+gendata_short <- data_frame(
+  tvec=seq(0, 21, by=0.1),
+  density=filter(intervention, Gratio==head(Gratio,1))$Rv*dgamma(tvec, shape=1/kappa, rate=1/kappa/(Gw*head(Gratio,1))),
+  strength=density/constant_strength,
+  speed=density*exp(-constant_speed*tvec)
+)
+
+Gpost_short <- integrate(function(x) x*filter(intervention, Gratio==head(Gratio,1))$Rv*dgamma(x, shape=1/kappa, rate=1/kappa/(Gw*head(Gratio,1)))*exp(-constant_speed*x),
+                        0,
+                        Inf)[[1]]/filter(intervention, Gratio==head(Gratio,1))$speed_post_strength
+
+g1 <- ggplot(gendata_short) +
+  geom_line(aes(tvec, density, col="Pre-intervention", lty="Pre-intervention"), lwd=3) +
+  geom_line(aes(tvec, strength, col="Post-intervention (constant-strength)", lty="Post-intervention (constant-strength)"), lwd=3) +
+  geom_vline(xintercept = (Gw*head(Gratio,1)), lwd=2) +
+  geom_vline(xintercept = (Gw*head(Gratio,1)), lwd=2, col=colorblind_pal()(3)[3], lty=3) +
+  scale_x_continuous("Generation intervals (days)") +
+  scale_y_continuous("Kernel density", limits=c(0, 0.45)) +
+  scale_color_manual("", values=(colorblind_pal()(3)[c(1, 3)]),
+                     breaks=c("Pre-intervention", "Post-intervention (constant-strength)")) +
+  scale_linetype_manual("", values=c(1, 3),
+                        breaks=c("Pre-intervention", "Post-intervention (constant-strength)")) +
+  ggtitle("A. Shorter generation interval") +
+  theme(
+    panel.grid = element_blank(),
+    legend.position = c(0.6, 0.95),
+    legend.background = element_rect(fill=NA),
+    axis.title = element_blank()
+  )
+
+g2 <- ggplot(gendata_short) +
+  geom_line(aes(tvec, density, col="Pre-intervention", lty="Pre-intervention"), lwd=3) +
+  geom_line(aes(tvec, speed, col="Post-intervention (constant-speed)", lty="Post-intervention (constant-speed)"), lwd=3) +
+  geom_vline(xintercept = (Gw*head(Gratio,1)), lwd=2) +
+  geom_vline(xintercept = Gpost_short, lwd=2, col=colorblind_pal()(3)[2], lty=2) +
+  scale_x_continuous("Generation intervals (days)") +
+  scale_y_continuous("Kernel density", limits=c(0, 0.45)) +
+  scale_color_manual("", values=(colorblind_pal()(3)[c(1, 2)]),
+                     breaks=c("Pre-intervention", "Post-intervention (constant-speed)")) +
+  scale_linetype_manual("", values=c(1, 2),
+                        breaks=c("Pre-intervention", "Post-intervention (constant-speed)")) +
+  ggtitle("B. Shorter generation interval") +
+  theme(
+    panel.grid = element_blank(),
+    legend.position = c(0.6, 0.95),
+    legend.background = element_rect(fill=NA),
+    axis.title = element_blank()
+  )
+
+g3 <- ggplot(gendata_base) +
   geom_line(aes(tvec, density, col="Pre-intervention", lty="Pre-intervention"), lwd=3) +
   geom_line(aes(tvec, strength, col="Post-intervention (constant-strength)", lty="Post-intervention (constant-strength)"), lwd=3) +
   geom_vline(xintercept = Gw, lwd=2) +
@@ -100,14 +149,15 @@ g1 <- ggplot(gendata_base) +
                      breaks=c("Pre-intervention", "Post-intervention (constant-strength)")) +
   scale_linetype_manual("", values=c(1, 3),
                         breaks=c("Pre-intervention", "Post-intervention (constant-strength)")) +
-  ggtitle("A. Equal generation intervals") +
+  ggtitle("C. Equal generation interval") +
   theme(
     panel.grid = element_blank(),
-    legend.position = c(0.6, 0.95),
-    legend.background = element_rect(fill=NA)
+    legend.position = "none",
+    legend.background = element_rect(fill=NA),
+    axis.title = element_blank()
   )
 
-g2 <- ggplot(gendata_base) +
+g4 <- ggplot(gendata_base) +
   geom_line(aes(tvec, density, col="Pre-intervention", lty="Pre-intervention"), lwd=3) +
   geom_line(aes(tvec, speed, col="Post-intervention (constant-speed)", lty="Post-intervention (constant-speed)"), lwd=3) +
   geom_vline(xintercept = Gw, lwd=2) +
@@ -118,39 +168,41 @@ g2 <- ggplot(gendata_base) +
                      breaks=c("Pre-intervention", "Post-intervention (constant-speed)")) +
   scale_linetype_manual("", values=c(1, 2),
                         breaks=c("Pre-intervention", "Post-intervention (constant-speed)")) +
-  ggtitle("B. Equal generation intervals") +
+  ggtitle("D. Equal generation interval") +
   theme(
     panel.grid = element_blank(),
-    legend.position = c(0.6, 0.95),
-    legend.background = element_rect(fill=NA)
+    legend.position = "none",
+    legend.background = element_rect(fill=NA),
+    axis.title = element_blank()
   )
 
-g3 <- ggplot(gendata_long) +
+g5 <- ggplot(gendata_long) +
   geom_line(aes(tvec, density), lwd=3, col=colorblind_pal()(3)[1]) +
   geom_line(aes(tvec, strength), lwd=3, col=colorblind_pal()(3)[3], lty=3) +
   geom_vline(xintercept = (Gw*tail(Gratio,1)), lwd=2) +
   geom_vline(xintercept = (Gw*tail(Gratio,1)), lwd=2, col=colorblind_pal()(3)[3], lty=3) +
   scale_x_continuous("Generation intervals (days)") +
   scale_y_continuous("Kernel density", limits=c(0, 0.4)) +
-  ggtitle("C. Longer generation intervals") +
+  ggtitle("E. Longer generation interval") +
   theme(
-    panel.grid = element_blank()
+    panel.grid = element_blank(),
+    axis.title = element_blank()
   )
 
-g4 <- ggplot(gendata_long) +
+g6 <- ggplot(gendata_long) +
   geom_line(aes(tvec, density), lwd=3, col=colorblind_pal()(3)[1]) +
   geom_line(aes(tvec, speed), lwd=3, col=colorblind_pal()(3)[2], lty=2) +
   geom_vline(xintercept = (Gw*tail(Gratio,1)), lwd=2) +
   geom_vline(xintercept = Gpost_long, lwd=2, col=colorblind_pal()(3)[2], lty=2) +
   scale_x_continuous("Generation intervals (days)") +
   scale_y_continuous("Kernel density", limits=c(0, 0.4)) +
-  ggtitle("D. Longer generation intervals") +
+  ggtitle("F. Longer generation interval") +
   theme(
-    panel.grid = element_blank()
+    panel.grid = element_blank(),
+    axis.title = element_blank()
   )
 
-
-g5 <- ggplot(intervention) +
+g7 <- ggplot(intervention) +
   geom_hline(yintercept=1, col="gray") +
   geom_line(aes(Gratio, Rv, col="Pre-intervention", lty="Pre-intervention"), lwd=3) +
   geom_line(aes(Gratio, strength_post_strength, col="Post-intervention (constant-strength)", lty="Post-intervention (constant-strength)"), lwd=3) +
@@ -163,15 +215,15 @@ g5 <- ggplot(intervention) +
                         breaks=c("Pre-intervention",  "Post-intervention (constant-speed)", "Post-intervention (constant-strength)")) +
   scale_color_manual("", values=(colorblind_pal()(3)),
                      breaks=c("Pre-intervention",  "Post-intervention (constant-speed)", "Post-intervention (constant-strength)")) +
-  ggtitle("E") +
+  ggtitle("G") +
   theme(
     panel.grid = element_blank(),
     legend.title = element_blank(),
-    legend.position = c(0.35, 0.85),
+    legend.position = c(0.35, 0.84),
     legend.background = element_rect(fill=NA)
   )
 
-g6 <- ggplot(intervention) +
+g8 <- ggplot(intervention) +
   geom_hline(yintercept=0, col="gray") +
   geom_line(aes(Gratio, rv, col="Epidemic", lty="Epidemic"), lwd=3) +
   geom_line(aes(Gratio, strength_post_speed, col="Post-intervention (constant-strength)", lty="Post-intervention (constant-strength)"), lwd=3) +
@@ -182,17 +234,16 @@ g6 <- ggplot(intervention) +
   scale_y_continuous("Epidemic speed, $r_{\\mathrm{var}}$ (1/days)") +
   scale_linetype_manual("", values=1:3) +
   scale_color_manual("", values=(colorblind_pal()(3))) +
-  ggtitle("F") +
+  ggtitle("H") +
   theme(
     panel.grid = element_blank(),
     legend.title = element_blank(),
     legend.position = "none"
   )
 
+gtot <- ggarrange(g1, g2, g3, g4, g5, g6, g7, g8, nrow=4, ncol=2, draw=FALSE)
 
-gtot <- ggarrange(g1, g2, g3, g4, g5, g6, nrow=3, ncol=2, draw=FALSE)
-
-tikz(file = "control.tex", width = 12, height = 10, standAlone = T)
+tikz(file = "control.tex", width = 12, height = 12, standAlone = T)
 gtot
 dev.off()
 tools::texi2dvi('control.tex', pdf = T, clean = T)
